76cbb0ec5d01e539528ff66c9ae51faf04c62fb0
super4mario69 <super4mario69@gmail.com>
2023-06-13 11:30:12 +0200

    Initial commit

---
+++ src/main/java/at/technikum/hemmingweight/HemmingWeightApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.hemmingweight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HemmingWeightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HemmingWeightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/hemmingweight/HemmingWeightApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.hemmingweight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HemmingWeightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
